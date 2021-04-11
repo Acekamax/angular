3f25179a92312c7deec3905db34b19233fb9e1c5
Daniel Rubio <dsrubio95@gmail.com>
2021-04-11 18:26:29 -0500

    carga con redireccion final a login desde cotizacion

--- src/app/cotizacion/cotizacion.component.ts
+++ src/app/cotizacion/cotizacion.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NzButtonSize } from 'ng-zorro-antd/button';
 import { ProductCartService } from '../services/firestore/product-carts.service';
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -13,7 +14,7 @@ export class CotizacionComponent implements OnInit {
   @Input()datacarrito:any;
   @Input()newcarCC:any;
   @Input()emailusuario:any;
-  constructor(private Pservice:ProductCartService) {
+  constructor(private Pservice:ProductCartService, private router:Router) {
    }
 
   size: NzButtonSize = 'large';
@@ -23,5 +24,6 @@ export class CotizacionComponent implements OnInit {
   cambiarStatusCarrito(){
     var data={status:"completed"}
     this.Pservice.update_carts(this.newcarCC.id,data)
+    this.router.navigateByUrl("login");
   }
 }
